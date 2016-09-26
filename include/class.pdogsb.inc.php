934f4caf69203ab8e7834aa4f4dae3158412f124
admin <admin@10.20.162.104>
2016-09-26 15:10:47 +0200

    Commit GSB 26/09

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -17,9 +17,9 @@
 
 class PdoGsb{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=gsb_frais';   		
-      	private static $user='root' ;    		
-      	private static $mdp='' ;	
+      	private static $bdd='dbname=imerrouch';   		
+      	private static $user='imerrouch' ;    		
+      	private static $mdp='Ixu0eebu' ;	
 		private static $monPdo;
 		private static $monPdoGsb=null;
 /**
