55f67c7ce5748222bb2d77b54552a4417485cc8b
admin <admin@10.20.162.105>
2016-09-29 23:31:40 +0200

    création de la vue_listeFiche, qui affiche les fiches des visiteurs et les mois a selectionner

---
+++ controleurs/c_validerFrais.php
@@ -0,0 +1,34 @@
+<?php
+
+$action = $_REQUEST['action'];
+switch ($action){
+    case 'selectionnerFiche':{
+        
+        include("vues/vue_listeFiche.php");
+        echo '</div>';
+        include("vues/vue_sommaire_comptable.php");
+            break;
+    }
+   
+    case 'validerFrais':{
+        $idVisiteur = $_SESSION['idVisiteur'];
+        $mois = $_SESSION['mois'];
+        $lesVisiteurs=$pdo->getLesVisiteurs();
+        $InfosFicheFrais=$pdo->getLesInfosFicheFrais($idVisiteur,$mois);
+        $dateModif=$InfosFicheFrais['dateModif'];
+        $libEtat=$InfosFicheFrais['libEtat'];
+        $montant=$InfosFicheFrais['montantValide'];
+        
+        $FraisForfait=$pdo->getLesFraisForfait($idVisiteur, $mois);
+        $FraisHorsForfait=$pdo->getLesFraisHorsForfait($idVisiteur, $mois);
+        
+        $pdo->validerFicheFrais($idVisiteur,$mois);
+        
+        include("vues/vue_listeFiche.php");
+        include("vues/vue_etapeFiche.php");
+        include("vues/vue_sommaire_comptable.php");
+            break;
+    }
+   }
+  ?>
+    
