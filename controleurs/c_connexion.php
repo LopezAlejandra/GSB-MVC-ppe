c349859cd7635d046360c131af52d663bff34307
admin <admin@10.20.162.105>
2016-09-29 22:44:26 +0200

    include vues_sommaire_Comptable si le profil est ="Comptable"

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -1,12 +1,5 @@
 <?php //fichier c_connexion.
-    if(!isset($_REQUEST['action']) && $fct->estConnecte() || // établir des actions lorsqu’on recupere le profil de l’utilisateur 
-($_REQUEST['action']=='accueilComptable') && $fct->estConnecte() ){
-        if($_SESSION['profil']=="Comptable"){
-            $_REQUEST['action']= 'accueilComptable';
-        }elseif($_SESSION['profil']=="Visiteur") {
-            $_REQUEST['action']= 'accueil';
-        }
-    }
+ 
     if(!isset($_REQUEST['action']) || $_REQUEST['action']!='valideConnexion' && !$fct->estConnecte()){
             $_REQUEST['action'] = 'demandeConnexion';
     }
@@ -22,7 +15,7 @@
                     $mdp = $_REQUEST['mdp'];
                     $visiteur = $pdo->getInfosVisiteur($login,$mdp);
                     if(!is_array( $visiteur)){
-                            $fct->ajouterErreur("<strong>Attention !</strong> Login ou mot de passe incorrect","alert-danger","");
+                            ajouterErreur(" Login ou mot de passe incorrect");
                             include("vues/v_connexion.php");
                             include("vues/v_contentErreurs.php");
                     } else {
@@ -30,34 +23,28 @@
                             $nom =  $visiteur['nom'];
                             $prenom = $visiteur['prenom'];
                             $profil = $visiteur['profil'];
-                            $fct->connecter($id,$nom,$prenom,$profil);
-                            $pdo->ajoutConnectionLog($id);//appel de la méthode ajoutConnectionLog qui enregistre les connexions des utilisateurs
-                            
-                            header('Location: '. URLBASE .'index.php');
-                            //header('Location: /index.php');
+                           $fct->connecter($id,$nom,$prenom,$profil);
+                          
+                               //Si l'utilisateur est un comptable, le systême affichera le 
+                    //sommaire dédié à ce dernier afin de lui permettre le suivi 
+                    //et la validation d'une fiche de frais. 
+                    //Si l'utilisateur est un visiteur, le sommaire lui permmettra 
+                    //la saisi et le suivi d'une fiche de frais
+                    if($profil=='comptable'){
+                        include("vues/vue_sommaire_comptable.php");
                     }
-                    break;
-            }
-            case 'accueil' :{
-                    header('Location: '. URLBASE . 'index.php?uc=gererProfil&action=monProfil');
-                    //header('Location: index.php?uc=gererProfil&action=monProfil');
-                    break;
-            }
-            case 'accueilComptable' :{
+                    else{
+                        include("vues/v_sommaire.php");
+                    } 
+                }
+                break;
+        }
+                
+	default :{
+		include("vue/v_connexion.php");
+		break;
+	}
+}
+?>
                     
-                    include("vues/v_sommaire.php");
-                    include("vues/v_accueilComptable.php");
-                    break;
-            }
-            case 'deconnexion' : {
-                    $fct->deconnecter();
-                    header('Location: '. URLBASE . 'index.php');
-                    //header('Location: index.php');
-                    break;
-            }
-            default :{
-                    include("vues/v_connexion.php");
-                    break;
-            }
-    }
-    ?>
+                   
