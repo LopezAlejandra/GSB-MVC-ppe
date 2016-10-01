4438bd8051d5e4eaa79a5ff48ec63f7f16428f34
admin <admin@10.20.162.105>
2016-10-01 17:27:16 +0200

    Ajout de commentaires

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -1,39 +1,41 @@
-<?php //fichier c_connexion.
- 
-    if(!isset($_REQUEST['action']) || $_REQUEST['action']!='valideConnexion' && !$fct->estConnecte()){
-            $_REQUEST['action'] = 'demandeConnexion';
-    }
-    $action = $_REQUEST['action'];
-
-    switch($action){
-            case 'demandeConnexion':{
+<?php 
+        if(!isset($_REQUEST['action'])){
+	$_REQUEST['action'] = 'demandeConnexion';
+}
+$action = $_REQUEST['action'];
+switch($action){
+	case 'demandeConnexion':{
+		include("vues/v_connexion.php");
+		break;
+	}
+	case 'valideConnexion':{
+            // l'application compare le login et le mot de passe saisis avec 
+            // les données login, mdp de la table visiteur dans la bdd pour vérifier si 
+            //l'utilisateur existe dans la base de données.
+                $login = $_REQUEST['login'];
+                $mdp = $_REQUEST['mdp'];
+                $visiteur= $pdo->getInfosVisiteur($login, $mdp);
+                if(!is_array($visiteur)){
+                    
+                    ajouterErreur("Login ou mot de passe incorrect");//Un message d'erreur s'affiche lorsque le login ou mdp est inconu
+                    include("vues/v_erreur.php");
                     include("vues/v_connexion.php");
-                    break;
-            }
-            case 'valideConnexion':{
-                    $login = $_REQUEST['login'];
-                    $mdp = $_REQUEST['mdp'];
-                    $visiteur = $pdo->getInfosVisiteur($login,$mdp);
-                    if(!is_array( $visiteur)){
-                            ajouterErreur(" Login ou mot de passe incorrect");
-                            include("vues/v_connexion.php");
-                            include("vues/v_contentErreurs.php");
-                    } else {
-                            $id = $visiteur['id'];
-                            $nom =  $visiteur['nom'];
-                            $prenom = $visiteur['prenom'];
-                            $profil = $visiteur['profil'];
-                           $fct->connecter($id,$nom,$prenom,$profil);
-                          
-                               //Si l'utilisateur est un comptable, le systême affichera le 
-                    //sommaire dédié à ce dernier afin de lui permettre le suivi 
-                    //et la validation d'une fiche de frais. 
-                    //Si l'utilisateur est un visiteur, le sommaire lui permmettra 
-                    //la saisi et le suivi d'une fiche de frais
-                    if($profil=='comptable'){
+                }
+                else{
+                 
+                    $id = $visiteur['id'];
+                    $nom =  $visiteur['nom'];
+                    $prenom = $visiteur['prenom'];
+                    $profil = $visiteur['profil'];
+                    connecter($id,$nom,$prenom,$profil);
+                    
+      
+                    if($profil=='Comptable'){// Si l'utilisateur est un comptable, le programme 
+                    //                       //affichera un sommaire qui concerne seulement les comptables
                         include("vues/vue_sommaire_comptable.php");
                     }
-                    else{
+                    else{//Sinon alors l'utilisateur est un visiteur,et le sommaire qui s'affichera 
+                         //sera ce qui est dédié seulement aux visiteurs.
                         include("vues/v_sommaire.php");
                     } 
                 }
@@ -41,10 +43,8 @@
         }
                 
 	default :{
-		include("vue/v_connexion.php");
+		include("vues/v_connexion.php");
 		break;
 	}
 }
-?>
-                    
-                   
+?>            
