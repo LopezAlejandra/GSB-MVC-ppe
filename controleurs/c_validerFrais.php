acf21af5abe59ac1aeebbf3f73ebc8d0de48e4f6
admin <admin@10.20.162.105>
2016-10-01 21:11:52 +0200

    case 'voirEtatFiche'

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -1,33 +1,64 @@
 <?php
-$action = $_REQUEST['action'];
+ include("vues/vue_sommaire_comptable.php");
+$action= $_REQUEST['action'];
 switch ($action){
     case 'selectionnerFiche':{
-        
+        $lesVisiteurs= $pdo->getLesVisiteurs();
+        $lesCles=array_keys($lesVisiteurs);
         include("vues/vue_listeFiche.php");
-        echo '</div>';
-        include("vues/vue_sommaire_comptable.php");
             break;
     }
-   
-    case 'validerFrais':{
+    case 'voirEtatFiche':{//
+        $mois = $_REQUEST['mois'];
+        $idVisiteur=$_REQUEST["lstVisiteur"];
+        $lesVisiteurs = $pdo->getLesVisiteurs();
+        conserverId($idVisiteur,$mois);
+        $lesVisiteursFiches=$pdo->getLesInfosFicheFrais($idVisiteur,$mois);
+       
+        if ($mois==" "){
+            ajouterErreur("... ");
+                include("vues/v_erreurs.php");  
+        }
+        else if(!is_array( $lesVisiteursFiches )){   
+            ajouterErreur("Pas de fiche de frais pour ce visiteur ce mois");
+                include("vues/v_erreurs.php");
+             
+        }
+        else{
+            $lesInfosFicheFrais=$pdo->getLesInfosFicheFrais($idVisiteur,$mois);
+            $dateModif=$lesInfosFicheFrais['dateModif'];
+            $libEtat=$lesInfosFicheFrais['libEtat'];
+            $montant=$lesInfosFicheFrais['montantValide'];
+        
+            $LesFraisForfait=$pdo->getLesFraisForfait($idVisiteur, $mois);
+            $LesFraisHorsForfait=$pdo->getLesFraisHorsForfait($idVisiteur, $mois);
+        
+        
+            include("vues/v_etatFrais.php");
+          
+        }
+            break;
+    }
+    case 'validerFiche':{
         $idVisiteur = $_SESSION['idVisiteur'];
         $mois = $_SESSION['mois'];
-        $lesVisiteurs=$pdo->getLesVisiteurs();
-        $InfosFicheFrais=$pdo->getLesInfosFicheFrais($idVisiteur,$mois);
-        $dateModif=$InfosFicheFrais['dateModif'];
-        $libEtat=$InfosFicheFrais['libEtat'];
-        $montant=$InfosFicheFrais['montantValide'];
+        $lesVisiteurs = $pdo->getLesVisiteurs();
         
-        $FraisForfait=$pdo->getLesFraisForfait($idVisiteur, $mois);
-        $FraisHorsForfait=$pdo->getLesFraisHorsForfait($idVisiteur, $mois);
+        $lesInfosFicheFrais=$pdo->getLesInfosFicheFrais($idVisiteur,$mois);
+        $dateModif=$lesInfosFicheFrais['dateModif'];
+        $libEtat=$lesInfosFicheFrais['libEtat'];
+        $montant=$lesInfosFicheFrais['montantValide'];
+        
+        $LesFraisForfait=$pdo->getLesFraisForfait($idVisiteur, $mois);
+        $LesFraisHorsForfait=$pdo->getLesFraisHorsForfait($idVisiteur, $mois);
         
         $pdo->validerFicheFrais($idVisiteur,$mois);
         
-        include("vues/vue_listeFiche.php");
-        include("vues/vue_etapeFiche.php");
-        include("vues/vue_sommaire_comptable.php");
+        
+        include("vues/v_etatFrais.php");
+       
+      
             break;
     }
-   }
-  ?>
-    
+}
+?>
