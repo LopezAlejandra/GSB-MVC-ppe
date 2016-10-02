88e244964863c6e3b559900d968a96e9bb391266
admin <admin@10.20.162.105>
2016-10-02 17:04:43 +0200

    État de fiche avant d'être validée

--- vues/v_etapeDeFiche.php
+++ vues/v_etapeDeFiche.php
@@ -1,6 +1,13 @@
-<?php
 
-//affichage du détail de la fiche de frais de ce 
-//visiteur pour ce mois (avec les frais forfaits 
-//et les frais hors forfait). 
-?>
+
+<!--affichage du détail de la fiche de frais de ce 
+visiteur pour ce mois (avec les frais forfaits 
+et les frais hors forfait). -->
+
+ <form action="index.php?uc=validerFrais&action=validerFiche" method="post">
+     <label>Fiche de Frais du <?php echo $mois;?> </label>  
+    <br/> État : <?php echo $libEtat?> depuis le <?php echo $dateModif ?>, 
+    montant validé : <?php echo $montantValide ?> €.<br/><br/> <!--état de fiche avant d'être validée-->
+        <input class="button" type="submit" value="Valider Fiche" /> <!--Valide la fiche-->
+ </form>
+                    
