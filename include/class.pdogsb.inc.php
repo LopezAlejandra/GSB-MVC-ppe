2fd6dcb9e01a1ec27e6f8c8942104d1752a0cd96
admin <admin@10.20.162.104>
2016-10-03 10:56:35 +0200

    v_etatFrais

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -17,7 +17,7 @@
 
 class PdoGsb{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=alopez';   		
+      	private static $bdd='dbname= alopez';   		
       	private static $user='alopez' ;    		
       	private static $mdp='dev9Chai' ;	
 		private static $monPdo;
