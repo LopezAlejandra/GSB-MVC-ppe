0202cfdd12e67e4638e9525892c2cad90d54b918
admin <admin@10.20.162.105>
2016-11-13 15:39:36 +0100

    Bouton Envoyer

--- controleurs/c_suivi_paiement.php
+++ controleurs/c_suivi_paiement.php
@@ -2,7 +2,7 @@
 $action = $_REQUEST['action'];
 $idUtilisateur = $_SESSION['idVisiteur'];
 switch($action){
-    case 'demandeSuiviPaiement':
+    case 'recupFichesValidees':
         $fichesfraisValidees=$pdo->getFichesFraisValidees();
        include("vues/v_suiviPaiement.php");//Fichier à créer
     break;
