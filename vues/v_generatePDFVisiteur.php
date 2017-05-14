3d9b3e7129e5dc3bc1c8da37fc1518472564a238
admin <admin@10.20.162.105>
2017-05-15 01:11:49 +0200

    Changements dans le PDF -Modification calcul du total -Modification de police et couleur du texte -Ajout d'image signature

--- vues/v_generatePDFVisiteur.php
+++ vues/v_generatePDFVisiteur.php
@@ -7,9 +7,12 @@ function creerPDFFiche($lesFraisHorsForfait,$lesFraisForfaits){
     require dirname(__DIR__) . DIRECTORY_SEPARATOR . "fpdf" . DIRECTORY_SEPARATOR . "fpdf.php";
     $pdf = new FPDF();
     $pdf->AddPage();
-    $pdf->Image("images/logo.jpg", 77, 10, 50, 36);
-    $pdf->SetFont('Arial', 'B', 24);
+    $pdf->Image("images/logo.jpg", 83, 10, 40, 28);
+    $pdf->Ln(-13);
+    $pdf->SetFont('Arial', 'B', 20);
+    $pdf->SetTextColor(16,80,122);
     $pdf->Cell(0, 100, utf8_decode("Remboursement de frais engages"), 0, 0, 'C');
+    $pdf->SetTextColor(0,0,0);
     $pdf->SetFont('Arial', '', 12);
     $pdf->Ln(60);
     //Fiche Visiteur
@@ -20,7 +23,8 @@ function creerPDFFiche($lesFraisHorsForfait,$lesFraisForfaits){
     $pdf->Cell(50, 10, $listeMois[date('n', strtotime("01-" . substr($mois, 4, 2) . '-' . substr($mois, 0, 4))) - 1 ] . ' ' . substr($mois, 0, 4), 0, 0, 'C');
     $pdf->Ln(20);
     // Frais forfaitaires
-    $pdf->SetFont('Arial', '', 10);
+    $pdf->SetTextColor(44,117,165);
+    $pdf->SetFont('Arial', 'I', 12);
     $pdf->Cell(45, 10, "Frais Forfaitaires", 1, 0, 'C');
     $pdf->Cell(45, 10, utf8_decode("Quantité"), 1, 0, 'C');
     $pdf->Cell(45, 10, "Montant Unitaire", 1, 0, 'C');
@@ -28,15 +32,21 @@ function creerPDFFiche($lesFraisHorsForfait,$lesFraisForfaits){
     $pdf->Ln(10);
     $totalFraisForfaits=0;
     foreach ($lesFraisForfaits as $fraisF){
-        $pdf->Cell(45,10,$fraisF['libelle'],1,0,'C');
+        $pdf->SetTextColor(0,0,0);
+         $pdf->SetFont('Arial', 'I', 10);
+        $pdf->Cell(45,10,utf8_decode($fraisF['libelle']),1,0,'L');
+         $pdf->SetFont('Arial', '', 10);
         $pdf->Cell(45,10,$fraisF['quantite'],1,0,'C');
         $pdf->Cell(45,10,$fraisF['montant'],1,0,'C');
-        $pdf->Cell(45,10,$totalFraisForfaits+=$fraisF['quantite']*$fraisF['montant']."€",1,0,'C');
+        $pdf->Cell(45,10,  ($fraisF['quantite']*$fraisF['montant']),1,0,'C');
         $pdf->Ln(10);
+        $totalFraisForfaits+=$fraisF['quantite']*$fraisF['montant'];
     }
     $totalFraisHorsForfait = 0;
-    $pdf->Ln(20);
+    $pdf->Ln(10);
     // Position en x avec 3 colonnes
+    $pdf->SetFont('Arial', 'I', 12);
+    $pdf->SetTextColor(44,117,165);
     $pdf->Cell(180,10,"Autres Frais",1,0,'C');
     $pdf->Ln(10);
     $pdf->Cell(60, 10, "Date", 1, 0, 'C');
@@ -44,7 +54,10 @@ function creerPDFFiche($lesFraisHorsForfait,$lesFraisForfaits){
     $pdf->Cell(60, 10, "Montant", 1, 0, 'C');
     $pdf->Ln(10);
     foreach($lesFraisHorsForfait as $fraisHF){
+         $pdf->SetTextColor(0,0,0);
+        $pdf->SetFont('Arial', 'I', 10);
         $pdf->Cell(60, 10, $fraisHF['date'], 1, 0, 'C');
+        $pdf->SetFont('Arial', '', 10);
         $pdf->Cell(60, 10, utf8_decode($fraisHF['libelle']), 1, 0, 'C');
         $pdf->Cell(60, 10, $fraisHF['montant'], 1, 0, 'C');
         $pdf->Ln(10);
@@ -56,18 +69,19 @@ function creerPDFFiche($lesFraisHorsForfait,$lesFraisForfaits){
     $pdf->Cell(50, 10, 'Total', 1, 0, 'C');
     $pdf->Cell(50, 10, $total, 1, 0, 'C');
     //Signature
-    $pdf->Ln(20);
+    $pdf->Ln(10);
     $pdf->SetX($pdf->_getpageformat('A4')[0] - 70);
     $pdf->Cell(50, 10, utf8_decode('Fait à Paris le ' . date('j') . ' ' . $listeMois[date('n') - 1] . ' ' . date('Y')));
-    $pdf->Ln(10);
+    $pdf->Ln(7);
     $pdf->SetX($pdf->_getpageformat('A4')[0] - 70);
     $pdf->Cell(50, 10, utf8_decode('Vu l\'agent comptable'));
-    $pdf->Ln(10);
+    $pdf->Ln(7);
     $pdf->SetX($pdf->_getpageformat('A4')[0] - 70);
     $pdf->Cell(50, 10, utf8_decode(strtoupper('signature')));
-    $pdf->Ln(5);
+    $pdf->Ln(7);
    //$pdf->Image("images/signature.jpg",40,40,40,36);
-    $pdf->Image('images/signature.jpg',30); 
+    $pdf->SetX($pdf->_getpageformat('A4')[0] - 70);
+    $pdf->Image('images/signature.jpg',115); 
     ob_end_clean();
     $pdf->Output();
 }
