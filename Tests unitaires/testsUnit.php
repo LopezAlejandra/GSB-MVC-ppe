4dd4cfd1df8cf1a8878af67f8b9afe455730f75f
admin <admin@10.20.162.105>
2017-05-15 01:08:30 +0200

    Ajout des tests: -Test getInfosVisiteur() -Test getVisiteursParDate -Test getLesInfosFichFrais -Tesy getLesIdFrais

--- Tests unitaires/testsUnit.php
+++ Tests unitaires/testsUnit.php
@@ -27,16 +27,68 @@ class testsUnit extends PHPUnit_Framework_TestCase{
     protected function setUp()
     {
 	$this->monPdotest = PdoGsb::getPdoGsb() ;
-    //    PdoGsb::getPdo()->exec("SET AUTOCOMMIT OFF ;");
-        PdoGsb::getPdo()->beginTransaction();
+//        PdoGsb::getPdo()->exec("SET AUTOCOMMIT OFF ;");
+        // PdoGsb::getPdo()->beginTransaction();
+    }
+    public function testgetInfosVisiteur(){
+        $visiteur = array (
+            "id" => "a131",
+            "nom" => "Villechalane",
+            "prenom" => "Louis",
+            "profil" => "Visiteur",
+            "0" => "a131",
+            "1" => "Villechalane",
+            "2" => "Louis",
+            "3" => "Visiteur"
+            );
+        $this->assertEquals($visiteur, $this->monPdotest->getInfosVisiteur('lvillachane', 'jux7g'));
+    }
+    
+   public function testgetVisiteursParDate(){
+        $lesVisiteursParDate=array(
+                                    0 =>array(
+                                        "id"=>"a131",
+                                        "nom"=>"Villechalane",
+                                        "prenom"=>"Louis",
+                                       ) ,
+                                    1=>array(
+                                        "id"=>"a55",
+                                       "nom"=>"Bedos",
+                                       "prenom"=>"Christian",
+                                       ));
+        $this->assertEquals($lesVisiteursParDate, $this->monPdotest->getVisiteursParDate("201705"));
     }
     
 
-    public function testgetNbjustificatifs() {
+   /** public function testgetNbjustificatifs() {
         $this->assertEquals(0, $this->monPdotest->getNbjustificatifs('a131', '201409'));
+    }**/
+
+ 
+    
+    public function testgetLesInfosFicheFrais(){
+        $fiche=array(
+            "idVisiteur"=>"a131",
+            "mois"=>"201705",
+            "idEtat"=>"CR",
+            "dateModif"=>"2017-05-14",
+            "nbJustificatifs"=>"0",
+            "montantValide"=>"0.00",
+            "libEtat"=>"Fiche créée, saisie en cours",
+            0=>"a131",
+            1=>"201705",
+            2=>"CR",
+            3=>"2017-05-14",
+            4=>"0",
+            5=>"0.00",
+            6=>"Fiche créée, saisie en cours"
+          );
+        $this->assertEquals($fiche, $this->monPdotest->getLesInfosFicheFrais("a131",'201705'));
+        
+        
     }
     
-    public function TestGetLesIdFrais(){
+    public function testgetLesIdFrais(){
         $lesId = array(
             0 => array (
                 'idfrais' =>  'ETP',
@@ -50,8 +102,9 @@ class testsUnit extends PHPUnit_Framework_TestCase{
             3 => array (
                 'idfrais' => 'REP',
                 0 => 'REP'));
-        $this->assertEquals($lesId, $this->monPdotest->GetLesIdFrais());
+        $this->assertEquals($lesId, $this->monPdotest->getLesIdFrais());
     }
+    
     
      
     
